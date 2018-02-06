6dd35ccb4610d6e4eb3631bc7a093b2fa85cf642
pratik-agarwal <36183667+pratik-agarwal@users.noreply.github.com>
2018-02-06 14:09:38 +0530

    Update draw_figure.py
    Added the comments to make changes

--- draw_figure.py
+++ draw_figure.py
@@ -4,10 +4,11 @@ def draw_square():
     window=turtle.Screen()
     window.bgcolor("green")
 
+    #picks the turtle/initialize the turtle object
     cue=turtle.Turtle()
     for i in range(1,37):
-        #picks the turtle
-        
+        #use the object 'cue' to create a square and turn the turtle 10 degrees to create a new
+        #square, thus creating a circle (36 times)
         cue.forward(100)
         cue.right(90)
         cue.forward(100)
